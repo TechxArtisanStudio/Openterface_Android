e22693f89501e0a97871ff81afc73f34c7c47698
Warren <teachwarren@163.com>
2024-09-23 15:08:58 +0800

    change one

--- app/src/main/java/com/openterface/AOS/target/MouseManager.java
+++ app/src/main/java/com/openterface/AOS/target/MouseManager.java
@@ -34,7 +34,7 @@ public class MouseManager {
     private static UsbDeviceManager usbDeviceManager;
     public static int screenWidth, screenHeight;
 
-    public static void width_height(int width, int height) {
+    public static void width_height(int width, int height) {//change one
         screenWidth = width;
         screenHeight = height;
     }
