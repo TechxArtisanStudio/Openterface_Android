7379281340b2530e5016a68adb288d027989f131
Chow411 <2604590430@qq.com>
2026-07-20 12:39:51 +0800

    Fix old device do not support

--- libuvccamera/src/main/jni/Android.mk
+++ libuvccamera/src/main/jni/Android.mk
@@ -1,8 +1,9 @@
 PROJ_PATH	:= $(call my-dir)
 
-# Enable 16KB page size support for Android 15+
-# Add linker flags for proper alignment
-LOCAL_LDFLAGS := -Wl,-z,max-page-size=16384
+# NOTE: LOCAL_LDFLAGS must be set in each sub-module AFTER include $(CLEAR_VARS),
+# not here. CLEAR_VARS wipes all LOCAL_* variables, so setting them before is dead code.
+# Each sub-module (UVCCamera, libjpeg-turbo, libyuv, libuvc, libusb) already has its own:
+#   LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 
 include $(CLEAR_VARS)
 include $(PROJ_PATH)/libjpeg-turbo/Android.mk
